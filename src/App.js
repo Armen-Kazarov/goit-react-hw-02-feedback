3ca7cc2e9a5d67632bfbd9a4c0655a3a25c1c146
Armen-Kazarov <dunkan_mc_laud@ukr.net>
2020-12-14 00:49:08 +0200

    add feedback components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import Feedback from './components/Feedback';
+import React, { Component } from 'react';
+import Section from './components/Section/Section';
+import { BUTTONS_OPTIONS } from './data/buttonsOptions';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Statistics from './components/Statistics/Statistics';
 import './App.css';
 
-function App() {
-  return (
-    <>
-      <Feedback />
-    </>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleOptions = ({ target }) => {
+    const { feedback } = target.dataset;
+    this.setState(prevState => ({
+      [feedback]: prevState[feedback] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+
+    return Math.round((good * 100) / (good + neutral + bad) || 0);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const countPositiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+    const onLeaveFeedback = this.handleOptions;
+
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={BUTTONS_OPTIONS}
+            onLeaveFeedback={onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={countPositiveFeedbackPercentage}
+          />
+        </Section>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import s from '../../components/FeedbackOptions/FeedbackOptions.module.css';
+
+const FeedbackButton = ({ feedback, onLeaveFeedback }) => {
+  return (
+    <button
+      type="button"
+      className={s.optionButton}
+      data-feedback={feedback}
+      onClick={onLeaveFeedback}
+    >
+      {feedback}
+    </button>
+  );
+};
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return options.map(option => (
+    <FeedbackButton feedback={option} onLeaveFeedback={onLeaveFeedback} />
+  ));
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string),
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
